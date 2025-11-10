6a08b45659a6abe5c7c68257c68ad8e0700ccd53
yulithalta <55260031+yulithalta@users.noreply.github.com>
2025-11-10 12:09:34 +0100

    Add additional service to up.sh

--- infrastructure/scripts/up.sh
+++ infrastructure/scripts/up.sh
@@ -7,3 +7,8 @@ docker compose --env-file ./.env -f ./docker-compose.yml ps
 echo ">> Zigbee2MQTT: http://localhost:8080"
 echo ">> Dozzle:      http://localhost:9090"
 echo ">> dashdot:     http://localhost:4000"
+echo ">> Camera Frontend: http://localhost:3000"
+echo ">> Camera Backend: http://localhost:3033"
+echo ">> Crudsensor App: http://localhost:3001"
+echo ">> Zigbee Dashboard: http://localhost:3002"
+echo ">> RGPD Module: http://localhost:3003"
